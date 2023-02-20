3eab51b7ddfc3e981fcfc38f103f80d560e1b46c
JoshuaDRose <joshuarose099@gmail.com>
2023-02-20 19:09:49 +1100

    Resolved #6

--- src/navbar.js
+++ src/navbar.js
@@ -5,10 +5,10 @@ function Navbar() {
     <div className="nav">
       <div className="nav-links">
         <ul>
-          <a class="content-link spaced-width" href="../public/index.html">Download</a>
-          <a class="content-link spaced-width" href="../public/index.html">Services</a>
-          <a class="content-link spaced-width" href="../public/index.html">About</a>
-          <a class="content-link spaced-width" href="../public/index.html">Contact</a>
+          <a className="content-link spaced-width" href="../public/index.html">Download</a>
+          <a className="content-link spaced-width" href="../public/index.html">Services</a>
+          <a className="content-link spaced-width" href="../public/index.html">About</a>
+          <a className="content-link spaced-width" href="../public/index.html">Contact</a>
         </ul>
       </div>
     </div>
