3062c235fd2b08ec67d69b4b5da6725e5094f3fa
JoshuaDRose <joshuarose099@gmail.com>
2023-02-20 18:49:39 +1100

    tests: added navbar test

--- src/tests/hero.test.js
+++ src/tests/hero.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import Hero from '../hero';
+import Navbar from '../navbar';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Navigation menu loads above hero', () => {
+  render(<Navbar />);
+  const link_dl = screen.getByText(/Download/i);
+  const link_sv = screen.getByText(/Services/i);
+  const link_ab = screen.getByText(/About/i);
+  const link_ct = screen.getByText(/Contact/i);
+  expect(link_dl).toBeInTheDocument();
+  expect(link_sv).toBeInTheDocument();
+  expect(link_ab).toBeInTheDocument();
+  expect(link_ct).toBeInTheDocument();
+});
+
+test('Symbols and text loaded without corrupting', () => {
+  render(<Hero />);
+  const heroText = screen.getByText(/MyBuy/i);
+  expect(heroText).toBeInTheDocument();
 });
